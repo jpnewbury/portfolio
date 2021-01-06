775170333e77ba6255bc7492c11a535a9d447e90
floydiology <john.newbury@gmail.com>
2021-01-05 17:56:09 -0700

    committed

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import Image from "next/image";
 class Home extends Component {
   constructor(props) {
     super(props);
@@ -6,8 +9,37 @@ class Home extends Component {
   }
   render() {
     return (
-      <div>
-        <h1> Hello</h1>
+      <div className="container">
+        <div className="center">
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={{
+              hidden: {
+                scale: 0.1,
+                opacity: 0,
+              },
+              visible: {
+                scale: 1,
+                opacity: 1,
+                transition: {
+                  delay: 0.45,
+                  duration: 0.65,
+                  // ease: [0.48, 0.15, 0.25, 0.96],
+                  type: "spring",
+                  stiffness: 100,
+                },
+              },
+            }}
+          >
+            <Image
+              src="/images/logo.png"
+              alt="Newbury Angling Arts and Design Portfolio"
+              width={300}
+              height={320}
+            />
+          </motion.div>
+        </div>
       </div>
     );
   }
