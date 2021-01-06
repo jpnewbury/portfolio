4d442c25b14527143069557f347c6d992ed36e99
floydiology <john.newbury@gmail.com>
2021-01-05 18:49:57 -0700

    committed

--- pages/index.js
+++ pages/index.js
@@ -23,14 +23,48 @@ export default function Home() {
       <div className="container">
         <div className="center">
           <div>
-            <div>
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              variants={{
+                hidden: {
+                  scale: 0.1,
+                  opacity: 0,
+                },
+                visible: {
+                  scale: 1,
+                  opacity: [
+                    0.3,
+                    0.7,
+                    0.3,
+                    0.7,
+                    0.9,
+                    1,
+                    0.3,
+                    1,
+                    0.3,
+                    0.4,
+                    0.5,
+                    0.9,
+                    0.2,
+                    1,
+                  ],
+                  transition: {
+                    delay: 0.05,
+                    duration: 0.65,
+                    type: "spring",
+                    stiffness: 220,
+                  },
+                },
+              }}
+            >
               <Image
                 src="/images/logo.png"
                 alt="Newbury Angling Arts and Design - Portfolio"
                 height={300}
                 width={300}
               />
-            </div>
+            </motion.div>
             <div>
               <Link href="/illustrate">
                 <a>
