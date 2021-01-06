8804030363ae08a858db340eb5b6ff49cf441880
floydiology <john.newbury@gmail.com>
2021-01-06 09:31:21 -0700

    january 6th code session

--- pages/illustrate/index.js
+++ pages/illustrate/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Newbury Angling Arts and Design - Portfolio</title>
+        <title>Newbury Angling Arts and Design - Illustration Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
         <meta
           property="og:title"
@@ -58,6 +58,122 @@ export default function Home() {
                 width={300}
               />
             </motion.div>
+          </div>
+        </div>
+        <div className="center1">
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={{
+              hidden: {
+                scale: 0.1,
+                opacity: 0,
+              },
+              visible: {
+                scale: 1,
+                opacity: 1,
+                transition: {
+                  delay: 0.15,
+                  duration: 0.65,
+                  type: "spring",
+                  stiffness: 120,
+                },
+              },
+            }}
+          >
+            <Image
+              src="/images/chiliCard.png"
+              alt="Newbury Angling Arts and Design - Portfolio"
+              height={700}
+              width={550}
+            />
+          </motion.div>
+        </div>
+        <div className="center1">
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={{
+              hidden: {
+                scale: 0.1,
+                opacity: 0,
+              },
+              visible: {
+                scale: 1,
+                opacity: 1,
+                transition: {
+                  delay: 0.25,
+                  duration: 0.5,
+                },
+              },
+            }}
+          >
+            <Image
+              src="/images/nymphs.png"
+              alt="Newbury Angling Arts and Design - Portfolio"
+              height={420}
+              width={300}
+            />
+          </motion.div>
+        </div>
+        <div className="center1">
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={{
+              hidden: {
+                scale: 0.1,
+                opacity: 0,
+              },
+              visible: {
+                scale: 1,
+                opacity: 1,
+                transition: {
+                  delay: 0.25,
+                  duration: 0.5,
+                },
+              },
+            }}
+          >
+            <Link href="/">
+              <a>
+                <Image
+                  src="/images/fineart.png"
+                  alt="Newbury Angling Arts and Design - Portfolio"
+                  height={840}
+                  width={500}
+                />
+              </a>
+            </Link>
+          </motion.div>
+        </div>
+        <div className="container">
+          <div className="col-3">
+            <div>
+              <Link href="/">
+                <a>
+                  <motion.img
+                    width="100px"
+                    src="/images/left_arrow.png"
+                    alt="Newbury Angling Arts and Design - Portfolio"
+                    whileHover={{
+                      position: "relative",
+                      zIndex: 1,
+                      scale: [1, 1.3, 1.1],
+                      rotate: [0, 10, -10, 0],
+                      transition: {
+                        duration: 0.2,
+                      },
+                    }}
+                  />
+                </a>
+              </Link>
+            </div>
+            <div></div>
+            <div>
+              {" "}
+              <img src="/images/right_arrow.png" width="100px" />
+            </div>
           </div>
         </div>
       </div>
