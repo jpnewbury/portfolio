2531b9f5329d37bb112c5ee8a2768fc3f8b238d7
floydiology <john.newbury@gmail.com>
2021-01-09 11:45:17 -0700

    commited

--- pages/sandbox/index.js
+++ pages/sandbox/index.js
@@ -1,54 +1,41 @@
-import React, { Component } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
 import { motion } from "framer-motion";
-class sandbox extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <>
-        <Head>
-          <title>Newbury Angling Arts and Design - CSS Sandboox</title>
-          <meta
-            property="og:title"
-            content="Newbury Angling Arts and Design - CSS Sandbox"
-            key="title"
-          />
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Newbury Angling Arts and Design - CSS Sandbox</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          property="og:title"
+          content="Newbury Angling Arts and Design - Portfolio"
+          key="title"
+        />
 
-          <meta
-            name="description"
-            content="A gallary of design, illustrations and front-end web development"
-          />
-        </Head>
-        <div className="container">
-          <div className="center">
-            <div className="stacked">
-              <div>
-                <Image
-                  src="/images/logo.png"
-                  alt="Newbury Angling Arts and Design - Portfolio"
-                  height={520}
-                  width={500}
-                />
-              </div>
-              <div>
-                <Image
-                  src="/images/btn_portfolio.png"
-                  alt="Newbury Angling Arts and Design - Portfolio"
-                  height={120}
-                  width={300}
-                />
-              </div>
-            </div>
+        <meta
+          name="description"
+          content="A gallary of design, illustrations and front-end web development"
+        />
+      </Head>
+
+      <div className="card-container">
+        <div className="center">
+          <div className="card">
+            <h1>MELLO</h1>
+          </div>
+          <div className="card1">
+            <h1>MELLO</h1>
+          </div>
+          <div className="card2">
+            <h1>MELLO</h1>
+          </div>
+          <div className="card3">
+            <h1>MELLO</h1>
           </div>
         </div>
-      </>
-    );
-  }
+      </div>
+    </div>
+  );
 }
-
-export default sandbox;
